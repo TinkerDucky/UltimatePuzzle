03ee8b7de3c6fc6cdf738c2302845c843b437336
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-06-11 18:22:03 +0200

    u

--- player.gd
+++ player.gd
@@ -3,6 +3,25 @@ extends Node2D
 var speed = 32
 var character 
 
+
+var move_dir = Vector2(1, 0)
+var player_orientation = 0
+
+func rotate_player_right():
+	var alpha = 0.5 * PI
+	player_orientation += alpha
+	move_dir = Vector2(move_dir.x * cos(alpha) + move_dir.y * (-sin(alpha)),
+					   move_dir.x * sin(alpha) + move_dir.y * cos(alpha))
+	print(move_dir)
+
+func rotate_player_left():
+	var alpha = -0.5 * PI
+	player_orientation += alpha
+	move_dir = Vector2(move_dir.x * cos(alpha) + move_dir.y * (-sin(alpha)),
+					   move_dir.x * sin(alpha) + move_dir.y * cos(alpha))
+	print(move_dir)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	(get_node("RigidBody2D/PlayerAnimation") as AnimatedSprite2D).play("run")
@@ -13,11 +32,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position.x += delta * speed
+	var velocity = move_dir * speed
+	self.position += velocity * delta
+	# self.position.x += delta * speed
 	pass
 
 
 func _on_rigid_body_2d_body_entered(body):
 	print("collision")
-	self.rotation = 3.1415 / 2
-	pass # Replace with function body.
+	self.position -= move_dir * speed * 0.2
+	rotate_player_right()
+	self.rotation = player_orientation
+	
